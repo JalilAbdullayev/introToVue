492a145701e150bc2c7eab6473352f5f3cb0f63e
Jalil <abdullacelil00@gmail.com>
2024-10-06 10:39:41 +0400

    Computed Properties

--- main.js
+++ main.js
@@ -3,41 +3,59 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       description: "A pair of warm, fuzzy socks.",
-      image: "./assets/images/socks_green.jpg",
+      selectedVariant: 0,
       url: "green-socks",
-      inStock: false,
       inventory: 100,
-      onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         {
           id: 2234,
           color: "green",
           image: "./assets/images/socks_green.jpg",
+          quantity: 50,
+          onSale: true,
         },
         {
           id: 2235,
           color: "blue",
           image: "./assets/images/socks_blue.jpg",
+          quantity: 0,
+          onSale: false,
         },
       ],
       sizes: ["S", "M", "L"],
       cart: 0,
       activeClass: true,
+      brand: "Vue Mastery",
     };
   },
   methods: {
     addToCart() {
       this.cart += 1;
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateVariant(index) {
+      this.selectedVariant = index;
     },
     removeFromCart() {
       this.cart -= 1;
       if (this.cart < 0) {
         this.cart = 0;
       }
+    },
+  },
+  computed: {
+    title() {
+      if (this.variants[this.selectedVariant].onSale) {
+        return this.brand + " " + this.product + " is on sale!";
+      } else {
+        return this.brand + " " + this.product;
+      }
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
     },
   },
 });
