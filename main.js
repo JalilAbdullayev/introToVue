d3064b519f431c966b524a8c34a0fe9adb6453e8
Jalil <abdullacelil00@gmail.com>
2024-10-06 09:40:12 +0400

    Event Handling

--- main.js
+++ main.js
@@ -13,13 +13,30 @@ const app = Vue.createApp({
         {
           id: 2234,
           color: "green",
+          image: "./assets/images/socks_green.jpg",
         },
         {
           id: 2235,
           color: "blue",
+          image: "./assets/images/socks_blue.jpg",
         },
       ],
       sizes: ["S", "M", "L"],
+      cart: 0,
     };
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    updateImage(variantImage) {
+      this.image = variantImage;
+    },
+    removeFromCart() {
+      this.cart -= 1;
+      if (this.cart < 0) {
+        this.cart = 0;
+      }
+    },
   },
 });
